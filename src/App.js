07dfe75ac5f6d56fb45b06883f1b091ef8628dcb
LASGLOWTECH <omozemojeau@gmail.com>
2023-05-12 11:20:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Hero from './components/Hero';
+
+import About from './components/About';
+import ContactUs from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
+  // console.log(process.env.REACT_APP_API_KEY);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <Router>
+    <div className='back'>
+    
+      
+      <Navbar/> 
+
+<div className='items '>
+<Switch>
+<Route exact path="/"> <Hero/></Route>
+<Route path="/About"> <About/>  </Route>
+<Route path="/Contact"> <ContactUs/>  </Route>
+
+
+ </Switch>
+ 
+    
+</div>
+<Footer/>
+
+</div>
+
+        </Router>
+ 
+  
     </div>
   );
 }
+
+
+
+      
+
+
 
 export default App;
---
+++ src/components/Project.js
@@ -0,0 +1,10 @@
+const Projects =()  =>{
+return (
+<div>
+    <p>You know what i am in</p>
+</div>
+
+);
+}
+
+export default Projects;
